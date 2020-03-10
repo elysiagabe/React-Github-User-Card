5d79dba3d0fe69fa9a493c1be88fe5191813d4ec
Elysia Gabe <elysiagabe@gmail.com>
2020-03-10 15:43:42 -0700

    Adds input and button to start stretch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends React.Component {
     super();
     this.state={
       userInfo: {},
-      followers: []
+      followers: [],
+      query: ''
     }
   }
 
@@ -46,11 +47,50 @@ class App extends React.Component {
       .catch(err => console.log('Oops! Problem fetching user\'s followers', err))
   }
 
+  handleInputChange = e => {
+    this.setState({
+      query: e.target.value
+    })
+  }
+
+  fetchUser = e => {
+    e.preventDefault();
+        // gets user's info & sets to state
+        axios.get(`https://api.github.com/users/${this.state.query}`)
+        .then(response => {
+          //console.log(response);
+          this.setState({
+            userInfo: response.data
+          });
+        })
+        .catch(err => console.log('Oops! Problem getting user info', err))
+  
+      // get user's followers & sets their info to state
+      axios.get(`https://api.github.com/users/${this.state.query}/followers`)
+        .then(response => {
+          //console.log(response.data)
+          let userFollowers = response.data;
+          userFollowers.map(follower => {
+            axios.get(`https://api.github.com/users/${follower.login}`)
+              .then(response => {
+                //console.log('These are followers:', response.data)
+                this.setState({
+                  followers: [...this.state.followers, response.data]
+                })
+                //console.log("This is followers state", this.state.followers)
+              })
+              .catch(err => console.log('Oops! Problem getting follower info:', err))
+          })
+        })
+        .catch(err => console.log('Oops! Problem fetching user\'s followers', err))
+  }
  
 
   render() {
     return (
       <div className="App">
+        <input value={this.state.query} onChange={this.handleInputChange}/>
+        <button onClick={this.fetchUser}>Search</button>
         <UserCard userInfo={this.state.userInfo} />
         <Followers followers={this.state.followers} />
       </div>
